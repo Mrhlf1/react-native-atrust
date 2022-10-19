6d82a71750c3334efa7d70f7c28403be830f8814
huang <466182060@qq.com>
2022-10-19 14:31:36 +0800

    android包名

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 
 import {
     NativeModules,
+    Platform
 } from 'react-native';
 
-const vpn = NativeModules.RNSangforVpn;
+// const vpn = NativeModules.RNSangforVpn;
+let vpn;
+if(Platform.OS === 'ios'){
+  vpn = NativeModules.RNSangforVpn;
+}else if (Platform.OS === 'android') {
+  vpn = NativeModules.RNSangforAtrustVpn
+}
 export default vpn;
 // export const Mode = {
 //   EasyApp: 0,
